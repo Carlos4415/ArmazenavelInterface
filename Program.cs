e15417e0e128f51e7cdf6acc3d3b52493ffbafea
Carlos4415 <carlos.soares4415@gmail.com>
2025-02-02 11:52:09 -0300

    Atualizando README

--- Program.cs
+++ Program.cs
@@ -1,11 +1,8 @@
-﻿/*
-    Este programa demonstra o uso de interfaces em C# para simular o armazenamento e recuperação de dados.
-    A interface IArmazenavel define os métodos Salvar e Recuperar, que são implementados nas classes Arquivo e BancoDeDados.
-    Cada classe fornece uma implementação específica dos métodos, simulando o processo de salvar e recuperar dados
-    em um arquivo e em um banco de dados. O objetivo é ilustrar o uso de interfaces para promover flexibilidade e
-    reutilização de código, permitindo a substituição de diferentes formas de armazenamento de dados em uma aplicação.
-*/
-
+﻿// Este programa demonstra o uso de interfaces em C# para simular o armazenamento e recuperação de dados.
+// A interface IArmazenavel define os métodos Salvar e Recuperar, que são implementados nas classes Arquivo e BancoDeDados.
+// Cada classe fornece uma implementação específica dos métodos, simulando o processo de salvar e recuperar dados
+// em um arquivo e em um banco de dados. O objetivo é ilustrar o uso de interfaces para promover flexibilidade e
+// reutilização de código, permitindo a substituição de diferentes formas de armazenamento de dados em uma aplicação.
 using ArmazenavelInterface;
 
 Arquivo arquivo = new();
